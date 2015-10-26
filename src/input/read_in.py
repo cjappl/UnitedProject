aa17d9cf81e419f7d46cbd11765056bf0db36e14
Apple, Christopher <christopher.j.apple@gmail.com>
2015-10-25 17:40:56 -0700

    Added _remove_headers functionality

--- src/input/read_in.py
+++ src/input/read_in.py
@@ -26,31 +26,33 @@ logger = get_united_logger()
 class UnitedPdfParser(object):
 
     def __init__(self, page_start):
+        self._page_start = page_start
+
         self._regex_groups = None
         self._pdf_path = PDF_PATH
         self._out_txt = OUT_TXT
         self._full_pdf_string = None
         self._regex = REGEX
-        self._page_start = page_start
 
     def input_pdf(self):
         self._create_txt_from_pdf()
         self._read_txt_into_str()
+        self._create_regex_groups()
 
-    def create_regex_groups(self):
+    def _create_regex_groups(self):
+        """ Creates the regex groups for cleaning. As it stands the groups will be:
+
+        1) name, 2) code, 3) optional extention, 4) distance 5) schedule string
+        """
+
         regex_groups_original = re.findall(REGEX, self._full_pdf_string)
 
         # The first 5 fields are most important
-        self._regex_groups = [group[:5] for group in regex_groups_original]
+        regex_groups_trimmed = [group[:5] for group in regex_groups_original]
         # 1) name, 2) code, 3) optional extention, 4) distance 5) schedule string
 
-        # TODO: REMOVE
-        pretty_str = pformat(self._regex_groups, width=70)
-        with open('pretty_str.txt', 'w') as f:
-            f.write(pretty_str)
-
-        pdb.set_trace()
-        #
+        regex_groups_no_headers = _remove_headers(regex_groups_trimmed)
+        
 
     def _create_txt_from_pdf(self):
         """ creates .txt file from pdf using system call to pdf2txt.py """
@@ -73,3 +75,94 @@ class UnitedPdfParser(object):
         with open(self._out_txt, 'r') as f:
             self._full_pdf_string = f.read()
         logger.info('SUCCESS!')
+
+
+def is_header_or_origin(regex_parsed_tuple):
+    """ Returns True if the tuple is of the form:
+    (origin_name, origin_code, ?, '', '')
+    (origin_name, origin_code, ?, 'Cont'd.', '')
+
+    This corresponds to an origin or header 
+    
+    Parameters
+    ----------
+    regex_parsed_tuple : tup
+        A len 5 tuple from a parsed United Pdf
+
+    Returns
+    -------
+    Bool : if last member is empty
+    """
+    # TODO: make private
+
+    if regex_parsed_tuple[-1] is '':
+        return True
+    else:
+        return False
+
+def is_destination(regex_parsed_tuple):
+    """ Returns True if the tuple is of the form:
+    (origin_name, origin_code, ?, '', 'schedule_info')
+
+    This corresponds to a destination member 
+
+    schedule_info strings must have | in them
+    
+    Parameters
+    ----------
+    regex_parsed_tuple : tup
+        A len 5 tuple from a parsed United Pdf
+
+    Returns
+    -------
+    Bool : if last member contains |  
+    """
+    # TODO: make private
+
+    if '|' in regex_parsed_tuple[-1]:
+        return True
+    else:
+        return False
+
+def _remove_headers(full_regex_list):
+    """ Removes headers from the full regex list
+
+    Used to prepare for final parsing
+
+    Parameters
+    ----------
+    full_regex_list : list of tup
+        A list from UnitedPdfParser containing the full regex group
+    
+    Returns
+    -------
+    regex_without_headers : list of tup
+        A list without headers. Headers will be found by:
+        1) iterating through list
+        2) if 3 headers or origins are found in a row, toss the first 2
+
+        Headers have been found to occur like this in the outputted regex groups 
+    """
+    i_to_remove = []
+    count = 0
+    pdb.set_trace()
+    for index, regex_group in enumerate(full_regex_list): 
+        if count == 3:
+            i_to_remove.append(index - 2)
+            i_to_remove.append(index - 1)
+
+        if is_header_or_origin(regex_group):
+            count += 1
+        else:
+            count = 0
+
+    pdb.set_trace()
+    regex_without_headers = [full_regex_list[i] for i in range(len(full_regex_list)) if i not in i_to_remove]
+
+
+    pdb.set_trace()
+    return full_regex_list
+        
+
+
+
--- tests/test_input.py
+++ tests/test_input.py
@@ -1,6 +1,14 @@
-from input.read_in import UnitedPdfParser 
+from input.read_in import * 
 import pytest
 import os
+
+ORIGIN = ('CHICAGO, IL ', 'ORD', " - O'HARE", "Cont'd.", '')
+HEADER = ('COLLEGE STATION, TX ', 'CLL', '', '', '')
+DESTINATION =  ('BERLIN, GERMANY ',
+  'TXL',
+  ' - TEGEL',
+  '288 mi',
+  '\n 6:25A   7:35A    2768    320 0   1h10m --MT-T-|-------|-------|-------\n 6:30A   7:40A    2768    320 0   1h10m -------|--MT---|--MT---|--MT---\n 6:30A   7:40A    2768    319 0   1h10m -------|-----T-|-----T-|-----T-\n 6:45A   7:55A    2757    319 0   1h10m S------|S------|-------|-------\n')
 
 
 @pytest.fixture
@@ -8,8 +16,22 @@ def parser():
     parser = UnitedPdfParser(109)
     return parser
 
+
 def test_pdf_exists(parser):
     assert os.path.isfile(parser._pdf_path)
+
+
+def test_is_header_or_origin():
+    assert is_header_or_origin(ORIGIN)
+    assert is_header_or_origin(HEADER)
+    assert not is_header_or_origin(DESTINATION)
+
+
+def test_is_destination():
+    assert is_destination(DESTINATION)
+    assert not is_destination(ORIGIN)
+    assert not is_destination(HEADER)
+
 
 @pytest.mark.long
 def test_create_txt_from_pdf(parser):
@@ -27,4 +49,6 @@ def test_input_pdf(parser):
 @pytest.mark.input_regex
 def test_input_regex(parser):
     parser.input_pdf()
-    parser.create_regex_groups()
+    parser._create_regex_groups()
+    list_of_reg = parser._regex_groups
+    assert len(list_of_reg)
